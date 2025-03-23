8e10c157be38bd16a7f5b23099d14a64e88f1dbc
ManhTrandev <manhttran121@gmail.com>
2025-03-23 15:09:36 +0700

    spring security

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -7,6 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
 @GetMapping("/")
 public String index() {
-return "Hello World from Spring Boot!";
+return "Hello World ";
+}
+@GetMapping("/user")
+public String userPage() {
+return "User page ";
+}
+@GetMapping("/admin")
+public String adminPage() {
+return "Admin page ";
 }
 }
+
