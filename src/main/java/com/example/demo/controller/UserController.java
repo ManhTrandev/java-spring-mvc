c9e4d4a4fb992432b0a928add98e8c58b060d016
ManhTrandev <manhttran121@gmail.com>
2025-03-24 13:54:08 +0700

    jsp

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
 
     public String getHomePage(){
         String test =this.userService.handleHello();
-        return "Viewshop.html";
+        return "hello";
     }
 }
 
