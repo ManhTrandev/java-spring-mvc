f806985b912e7b81ebec7fadc00d79de2494a88f
ManhTrandev <manhttran121@gmail.com>
2025-03-24 13:29:25 +0700

    static

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,33 +1,41 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.UserService;
 
 
-// @Controller
-// public class UserController {
-//     @RequestMapping("/")
-
-//     public String getHomePage(){
-//         return "HomePage from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
-    private UserService userService;
 
+    // DI
+    private UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/")
 
     public String getHomePage(){
-        return this.userService.handleHello();
+        String test =this.userService.handleHello();
+        return "Viewshop.html";
     }
 }
+
+// @RestController
+// public class UserController {
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("")
+
+//     public String getHomePage(){
+//         return this.userService.handleHello();
+//     }
+// }
