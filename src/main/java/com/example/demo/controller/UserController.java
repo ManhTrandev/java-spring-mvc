ab9ebdb388fe2bbe56ab2017fe20ddf0503af326
ManhTrandev <manhttran121@gmail.com>
2025-03-25 12:24:49 +0700

    create user and send data with htmlform

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,10 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
 
 
@@ -25,19 +26,16 @@ public class UserController {
         model.addAttribute("Manh", test);
         return "hello";
     }
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model){
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model mode,@ModelAttribute("newUser") User newInfor){
+        System.out.println(">>> Run"+newInfor);
+        return "hello";
+    }
 }
 
-// @RestController
-// public class UserController {
-//     private UserService userService;
-
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-//     @GetMapping("")
-
-//     public String getHomePage(){
-//         return this.userService.handleHello();
-//     }
-// }
